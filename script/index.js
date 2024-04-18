796d695b877422d83dd7fe2d788dd30565cf49ed
rafael-castilho-costa <rafael.castilho@tecnologia.blue>
2024-04-18 15:48:36 -0300

    fix(version-1.1.1):adjust #111

---
+++ script/index.js
@@ -0,0 +1,10 @@
+function sendEmail(){
+   const name = document.getElementById('nome');
+   const email = document.getElementById('email');
+   const mensagem = document.getElementById('mensagem');
+
+   console.log(name.value);
+   console.log(email.value);
+   console.log(mensagem.value);
+    
+}
